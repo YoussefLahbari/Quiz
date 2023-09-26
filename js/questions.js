9321326d5ab823dd293882a9d17fed8819136b66
Youssef <118392925+YoussefLahbari@users.noreply.github.com>
2023-09-26 19:44:10 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Wie alt bin ich?",
+    answer: "20 Jahre alt",
+    options: [
+      "19 Jahre alt",
+      "20 Jahre alt",
+      "21 Jahre alt",
+      "27 Jahre alt"
+    ]
+  },
+  {
+  numb: 2,
+  question: "Woher komme ich?",
+  answer: "Marokko",
+  options: [
+    "Deutschland",
+    "Syrien",
+    "Frankreich",
+    "Marokko"
+  ]
+},
+    {
+    numb: 3,
+    question: "Welche Sprachen spreche ich?",
+    answer: "Englisch-Deutsch-Französisch-Arabisch",
+    options: [
+      "Englisch-Deutsch-Türkisch-Arabisch",
+      "Englisch-Deutsch-Iranisch-Arabisch",
+      "Englisch-Deutsch-Französisch-Arabisch",
+      "Englisch-Deutsch-Französisch-Spanisch"
+    ]
+  },
+  {
+  numb: 4,
+  question: "Was war mein Abiturbereich?",
+  answer: "Elektro Wissenschaft und technik",
+  options: [
+    "Informatik und büromanagement",
+    "Physik und Chemie",
+    "Literatur und Philosophie",
+    "Elektro Wissenschaft und technik"
+  ]
+},
+    {
+    numb: 5,
+    question: "Was ist mein aktuelles Tätigkeit?",
+    answer: "Ausbildung Digitale entwicklung",
+    options: [
+      "Ausbildung büromanagement",
+      "Studium der Physik",
+      "Ausbildung Digitale entwicklung",
+      "Abitur Elektro-technik"
+    ]
+  }
+];
